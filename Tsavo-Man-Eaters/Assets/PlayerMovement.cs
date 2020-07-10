6c0bc62e08b65d4580eccf89a2412df106f5285a
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-07-10 16:41:45 -0500

    added some great stuff

--- Tsavo-Man-Eaters/Assets/PlayerMovement.cs
+++ Tsavo-Man-Eaters/Assets/PlayerMovement.cs
@@ -15,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
       //input
+      
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
