3eb0329cebf28f3c252377b97750b3f7fc4cfdfe
asaHorn <horntx3@gmail.com>
2020-07-10 17:26:10 -0500

    Setup Guards and waypoints

---
+++ Tsavo-Man-Eaters/Assets/GuardMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMovement : MonoBehaviour
+{
+  public Rigidbody2D[] rb;
+  public float guardSpeed = 2f;
+  public Rigidbody2D[] waypoints;
+  Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start() {}
+    void FixedUpdate()
+    {
+        //PlayerMovement
+        rb.MovePosition(waypoints[1].position * moveSpeed * Time.fixedDeltaTime);
+    }
+}
