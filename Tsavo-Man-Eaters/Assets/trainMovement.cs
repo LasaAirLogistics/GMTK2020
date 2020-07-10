d46fed4fb3176ee60523b6a453d3a4b8b960def6
prestonThiele <57765446+prestonThiele@users.noreply.github.com>
2020-07-10 16:33:48 -0500

    train
    choo choo

--- Tsavo-Man-Eaters/Assets/trainMovement.cs
+++ Tsavo-Man-Eaters/Assets/trainMovement.cs
@@ -7,14 +7,14 @@ public class trainMovement : MonoBehaviour
   void Start()
 {
 // Changes the position to x:1, y:1, z:0
-transform.position = new Vector3(1, 1, 0);
+//transform.position = new Vector3(1, 1, 0);
 // It is also possible to set the position with a Vector2
 // This automatically sets the Z axis to 0
-transform.position = new Vector2(1, 1);
+//transform.position = new Vector2(1, 1);
 // Moving object on a single axis
-Vector3 newPosition = transform.position; // We store the current position
-newPosition.y = 100; // We set a axis, in this case the y axis
-transform.position = newPosition; // We pass it back
+//Vector3 newPosition = transform.position; // We store the current position
+//newPosition.y = 100; // We set a axis, in this case the y axis
+//transform.position = newPosition; // We pass it back
 }
 private void Update()
 {
