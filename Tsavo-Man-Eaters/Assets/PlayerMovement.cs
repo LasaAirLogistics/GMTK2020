7dd3f1347ab80da6ed860acf28df19e74567875f
Ashton Wong <ashwong8@hotmail.com>
2020-07-10 17:25:04 -0500

    a

--- Tsavo-Man-Eaters/Assets/PlayerMovement.cs
+++ Tsavo-Man-Eaters/Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -17,6 +18,11 @@ public class PlayerMovement : MonoBehaviour
       //input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
     void FixedUpdate()
