570170bc966134a463b0a101d8be1fe917c5ce43
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-07-11 15:57:19 -0500

    tehe

--- Tsavo-Man-Eaters/Assets/Scripts/Score.cs
+++ Tsavo-Man-Eaters/Assets/Scripts/Score.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Score : MonoBehaviour
 {
-    public static int scoreValue = 0;
-    Text score;
+    public static string scoreValue;
+    public Text score;
+    public string scoreText;
+    public string outOf;
+    public int updateScore;
 
     // Start is called before the first frame update
     void Start()
     {
+      //Convert.ToInt32
         score = GetComponent<Text>();
+        scoreText = score.text;
+        outOf= scoreText.Substring(scoreText.Length - 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        score.text = "Kills: " + scoreValue;
+        updateScore = updateScore + 1;
+        score.text = "Kills: " + updateScore + "/" + outOf;
     }
 }
