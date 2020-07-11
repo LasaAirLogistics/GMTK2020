c6907069d037f1fe6e7e532b095543bd5b970002
Ashton Wong <ashwong8@hotmail.com>
2020-07-11 12:07:15 -0500

    m for mute

---
+++ Tsavo-Man-Eaters/Assets/Mute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mute : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+      audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+      if (Input.GetKeyDown("m"))
+          audioSource.mute = !audioSource.mute;
+    }
+}
