b7ea714e5a08a8a91752ee860ce81acfb0c1d68e
asaHorn <horntx3@gmail.com>
2020-07-10 23:59:34 -0500

    Working cone and movement. No rot

---
+++ Tsavo-Man-Eaters/Assets/ConeMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeMove : MonoBehaviour
+{
+    public int coneDist;
+    public Transform myGuard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(myGuard.position.x + coneDist, myGuard.position.y);
+    }
+}
--- Tsavo-Man-Eaters/Assets/GuardMovement.cs
+++ Tsavo-Man-Eaters/Assets/GuardMovement.cs
@@ -7,19 +7,30 @@ public class GuardMovement : MonoBehaviour
   public Rigidbody2D guardBody;
   public float guardSpeed = 2f;
   public Transform[] waypoints; //array of waypoints (MUST BE IN ORDER)
+  private int curPoint = 0;
 
     // Start is called before the first frame update
     void Start(){
       //avoid trying to pathfind between one way point
-      if(!(waypoints.Length > 1))
+      if(waypoints.Length <= 1)
       {
         Debug.LogError("No path given, exiting.", gameObject);
       }
+
+      //GuardMovement
+      transform.position = waypoints[0].position;
     }
 
     void FixedUpdate()
     {
-        //PlayerMovement
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[1].position, guardSpeed * Time.fixedDeltaTime);
+      //GuardMovement
+      transform.position = Vector2.MoveTowards(transform.position, waypoints[curPoint].position, guardSpeed * Time.fixedDeltaTime);
+
+        if(Vector3.Distance(waypoints[curPoint].position, transform.position) < .7){ //check if we finished last move
+          curPoint = curPoint + 1;
+          if(curPoint >= waypoints.Length){
+            curPoint = 0;
+          }
+        }
     }
 }
