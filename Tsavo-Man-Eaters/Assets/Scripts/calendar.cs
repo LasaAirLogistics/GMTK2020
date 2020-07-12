681447b859e0433c870072c0fa5a1c420bd4f2e7
asaHorn <horntx3@gmail.com>
2020-07-11 21:05:36 -0500

    Added calendar(persistent)
    Calender tracks the current day

---
+++ Tsavo-Man-Eaters/Assets/Scripts/calendar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class calendar : MonoBehaviour
+{
+  private int day = 0;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    DontDestroyOnLoad(this.gameObject);
+    SceneManager.activeSceneChanged += ChangedActiveScene;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private void ChangedActiveScene(Scene current, Scene next){
+    Debug.Log("LoadScene: " + next.name, gameObject);
+    day++;
+    if(next.name != "GameOver"){
+      DontDestroyOnLoad(this.gameObject);
+    } else {
+      Destroy(this.gameObject);
+    }
+  }
+}
