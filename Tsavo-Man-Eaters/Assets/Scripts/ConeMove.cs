efa8cbe2b05a1ff43bb4585dc719d851834df6fd
asaHorn <horntx3@gmail.com>
2020-07-11 20:24:59 -0500

    Make light track with guard

--- Tsavo-Man-Eaters/Assets/Scripts/ConeMove.cs
+++ Tsavo-Man-Eaters/Assets/Scripts/ConeMove.cs
@@ -6,6 +6,7 @@ public class ConeMove : MonoBehaviour
 {
     public float coneDist = -1.8f; //the distance the cone should float from the guardBody
     public Transform myGuard; //The guard we are paired with
+    public int rotDistance = 90; //how far to rotate  the object while tracking with guardBody
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,6 @@ public class ConeMove : MonoBehaviour
     void Update()
     {
         transform.position = myGuard.position + transform.up * coneDist; //everyupdate teleport me to myGuard
-        transform.rotation = myGuard.rotation * Quaternion.Euler(0,0,90); //set the right rotation
+        transform.rotation = myGuard.rotation * Quaternion.Euler(0,0,rotDistance); //set the right rotation
     }
 }
