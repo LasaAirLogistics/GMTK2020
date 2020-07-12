90432c4dfc4763646190e42180a670a8828e5028
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-07-11 20:51:59 -0500

    Update lairToNextLevel.cs

--- Tsavo-Man-Eaters/Assets/Scripts/lairToNextLevel.cs
+++ Tsavo-Man-Eaters/Assets/Scripts/lairToNextLevel.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class lairToNextLevel : MonoBehaviour
 {
-
-  private AssetBundle myLoadedAssetBundle;
-  private string[] scenePaths;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+  public string scoreText;
+  public bool finished;
 
     // Update is called once per frame
     public void OnTriggerEnter2D(Collider2D lair)
     {
-      if(lair.gameObject.tag == "Lair"){
+      GameObject canvas = GameObject.Find("Canvas");
+      Text[] textValue = canvas.GetComponentsInChildren<Text>();
+      scoreText = textValue[0].text;
+      if(scoreText == "Go back to den!"){
+        finished = true;
+      }
+      if(lair.gameObject.tag == "Lair" && finished){
             SceneManager.LoadScene("Day2");
     }
   }
