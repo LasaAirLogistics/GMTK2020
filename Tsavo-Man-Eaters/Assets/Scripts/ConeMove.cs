82143df893fcbcf7a2b557c0af35387accc39c1c
asaHorn <horntx3@gmail.com>
2020-07-11 20:36:55 -0500

    guard hit detection

--- Tsavo-Man-Eaters/Assets/Scripts/ConeMove.cs
+++ Tsavo-Man-Eaters/Assets/Scripts/ConeMove.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ConeMove : MonoBehaviour
 {
-    public float coneDist = -1.8f; //the distance the cone should float from the guardBody
-    public Transform myGuard; //The guard we are paired with
-    public int rotDistance = 90; //how far to rotate  the object while tracking with guardBody
+  public float coneDist = -1.8f; //the distance the cone should float from the guardBody
+  public Transform myGuard; //The guard we are paired with
+  public int rotDistance = 90; //how far to rotate  the object while tracking with guardBody
 
-    // Start is called before the first frame update
-    void Start()
-    {
+  // Start is called before the first frame update
+  void Start(){}
 
+  // Update is called once per frame
+  void Update()
+  {
+      transform.position = myGuard.position + transform.up * coneDist; //everyupdate teleport me to myGuard
+      transform.rotation = myGuard.rotation * Quaternion.Euler(0,0,rotDistance); //set the right rotation
+  }
+
+  private void OnTriggerEnter2D(Collider2D other){
+    if(other.gameObject.tag == "Lion"){
+      Debug.Log("HIT");
+      SceneManager.LoadScene("GameOver");
+            Time.timeScale = 0;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = myGuard.position + transform.up * coneDist; //everyupdate teleport me to myGuard
-        transform.rotation = myGuard.rotation * Quaternion.Euler(0,0,rotDistance); //set the right rotation
-    }
+  }
 }
