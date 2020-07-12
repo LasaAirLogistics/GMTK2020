7b9bae03e78ade9209de89ff40d25756447ca4ca
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-07-11 20:43:54 -0500

    scene switch

---
+++ Tsavo-Man-Eaters/Assets/Scripts/lairToNextLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class lairToNextLevel : MonoBehaviour
+{
+
+  private AssetBundle myLoadedAssetBundle;
+  private string[] scenePaths;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter2D(Collider2D lair)
+    {
+      if(lair.gameObject.tag == "Lair"){
+            SceneManager.LoadScene("Day2");
+    }
+  }
+}
