290e54cc8124f683773bed9a17c9d50cc0601618
asaHorn <horntx3@gmail.com>
2020-07-11 21:10:00 -0500

    Update calendar.cs
    added comments like a good code monkey

--- Tsavo-Man-Eaters/Assets/Scripts/calendar.cs
+++ Tsavo-Man-Eaters/Assets/Scripts/calendar.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 
 public class calendar : MonoBehaviour
 {
-  private int day = 0;
+  private int day = 0; //variable which stores the day
 
   // Start is called before the first frame update
   void Start()
   {
-    DontDestroyOnLoad(this.gameObject);
-    SceneManager.activeSceneChanged += ChangedActiveScene;
+    DontDestroyOnLoad(this.gameObject); //because for some reason loading in isn't a scene change
+    SceneManager.activeSceneChanged += ChangedActiveScene; //subscribe to ChangedActiveScene
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void Update(){} //efficent programing
 
-  }
-
-  private void ChangedActiveScene(Scene current, Scene next){
+  private void ChangedActiveScene(Scene current, Scene next){ //is called on scene change
     Debug.Log("LoadScene: " + next.name, gameObject);
-    day++;
+    day++; //up the day
     if(next.name != "GameOver"){
-      DontDestroyOnLoad(this.gameObject);
+      DontDestroyOnLoad(this.gameObject); //refresh our persistant status
     } else {
-      Destroy(this.gameObject);
+      Destroy(this.gameObject); // reset the counter if the game ends
     }
   }
 }
